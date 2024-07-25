16ba57c5aade14c44bc0e21d0d7de1470129162f
Fabio Rodrigues <fabio.rod@outlook.pt>
2024-07-25 18:51:02 +0100

    Base routing

---
+++ src/main/subscriptions/subscriptions.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "./contacts.style.css";
+
+export const Subscriptions = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
