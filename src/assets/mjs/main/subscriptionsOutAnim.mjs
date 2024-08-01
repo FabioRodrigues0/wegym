9a1a301cbeae8d94df30f09e658b458aaad1ea6e
JoaoNuno96 <joaonuno1996@hotmail.com>
2024-08-01 15:41:55 +0100

    corrigir falhas na animação

--- src/assets/mjs/main/subscriptionsOutAnim.mjs
+++ src/assets/mjs/main/subscriptionsOutAnim.mjs
@@ -13,16 +13,16 @@ export default function unFocuSub(card)
 
     itemCard.style.border = "none";
     itemCard.style.shadow = "";
-    itemCard.style.transition = "all 0,5s";
+    itemCard.style.transition = "all 0.5s";
     itemCard.style.boxShadow = ""; //RGB #fff7e5
 
     itemCardHeader.style.background = "transparent";
-    itemCardHeader.style.transition = "all 0,5s";
+    itemCardHeader.style.transition = "all 0.5s";
     itemCardTitle.style.color = "#FFB200";
 
     
     itemCardHeader.style.color = "#FFF";
 
     itemCardFooter.style.background = "transparent";
-    itemCardFooter.style.transition = "all 0,5s";
+    itemCardFooter.style.transition = "all 0.5s";
   }
