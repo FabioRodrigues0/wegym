0f37381edfdca8df6c093783353d0c74cf10144c
JoaoNuno96 <joaonuno1996@hotmail.com>
2024-08-03 14:59:55 +0100

    Corrigir botão do carrousel

--- src/modules/carrocel/carrocel.jsx
+++ src/modules/carrocel/carrocel.jsx
@@ -20,12 +20,12 @@ export default function Carrocel() {
                 </div>
             </div>
             <button className="carousel-control-prev" type="button"
-                    data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                    data-bs-target="#banner-carousel" data-bs-slide="prev">
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span className="visually-hidden">Previous</span>
             </button>
             <button className="carousel-control-next" type="button"
-                    data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                    data-bs-target="#banner-carousel" data-bs-slide="next">
                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
                 <span className="visually-hidden">Next</span>
             </button>
