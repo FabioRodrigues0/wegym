b871c1c40bd9bc9046e554224e069f496e2ac87a
JoaoNuno96 <joaonuno1996@hotmail.com>
2024-08-09 17:49:11 +0100

    Dropdown button ajustado e erros resolvidos

--- src/modules/badgeUser/badgeUser.jsx
+++ src/modules/badgeUser/badgeUser.jsx
@@ -6,23 +6,24 @@ const BadgeUser = (props) => {
     const showState = props.show;
     const nameEmail = props.email.split("@");
     const emailInitial = nameEmail[0] ? nameEmail[0][0] : "";
-    const colors = ["bg-primary", "bg-success", "bg-info", "bg-info", "bg-warning", "bg-danger"];
-    const randomIndex = Math.floor(Math.random() * colors.length);
+    // const colors = ["bg-primary", "bg-success", "bg-info", "bg-info", "bg-warning", "bg-danger"];
+    // const randomIndex = Math.floor(Math.random() * colors.length);
 
-    const classNames = ("avatar-circle rounded-circle text-end " + colors[randomIndex])
+    const classNames = ("avatar-circle rounded-circle bg-warning text-end")
     console.log(classNames);
     return (
         <div className="avatar" id="logoUser">
+
             {/*{(showState) ? classNames : classNames + " d-none"}*/}
-            <span id="spanBadge" className={(showState) ? classNames : classNames + " d-none"}>
+            <span id="spanBadge" className={(showState) ? classNames : `${classNames} d-none`}>
                 {emailInitial.toLocaleUpperCase()}
             </span>
-            <div id="dropdownBadge"  class="dropdown d-none">
-                <button class="btn btn-secondary dropdown-toggle bg-dark card-no-border" type="button" data-bs-toggle="dropdown"
+            <div id="dropdownBadge"  className="dropdown d-none ">
+                <button className="btn btn-secondary dropdown-toggle bg-dark card-no-border" type="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
                 </button>
-                <ul class="dropdown-menu bg-dark">
-                    <li><a onClick={logOff} className="btn btn-warning-outline m-0 text-warning">Log out</a></li>
+                <ul className="dropdown-menu bg-dark dropdown-menu-lg-end">
+                    <li><a onClick={logOff} className="btn btn-warning-outline m-0 text-warning dropdown-item">Log out</a></li>
                 </ul>
             </div>
         </div>
